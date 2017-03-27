7426d385a98f6785804321b58566cb3ea25022c1
qiuhengbin <qiuhengbin@star-net.cn>
2017-03-27 20:46:32 +0800

    BUGFIX 修复项目代码格式

--- imooc-crawler.js
+++ imooc-crawler.js
@@ -1,10 +1,19 @@
-var http = require ('http')
-var cheerio = require('cheerio')
-var url = 'http://www.imooc.com/learn/348'
+'use strict';
 
+// 用JS的严格模式，保证语法的严格性，强化 JS 语法的 若类型 风格，向 Java 看齐
+// 用 const let 替换 var，不再使用 var
+const http = require('http');
+const cheerio = require('cheerio');
+
+const url = 'http://www.imooc.com/learn/348';
+
+/**
+ * 从 HTML 中过滤课程信息
+ * @param html 网页中的 HTML
+ */
 function filterChapters(html) {
-  $ = cheerio.load(html)
-  var chapters = $('.chapter')
+  const $ = cheerio.load(html);
+  const chapters = $('.chapter');
 
   // [{
  //   chapterTitle: '',
@@ -14,54 +23,61 @@ function filterChapters(html) {
  //   ]
  // }]
 
- var courseData
- chapters.each(function(item) {
-   var chapter = $(this)//每一个单独的dom
-   var chapterTitle = chapter.find('strong').text()
-   var videos = chapter.find('.video').children('li')
+  const courseData = [];
 
-   var chapterData = {
-      chapterTitle: chapterTitle,
+  chapters.each(() => {
+    const chapter = $(this);       // 每一个单独的dom
+    const chapterTitle = chapter.find('strong').text();
+    const videos = chapter.find('.video').children('li');
+
+    const chapterData = {
+      chapterTitle,
       videos: []
-    }
+    };
 
-    videos.each(function(item) {
-      var video = $(this).find('J-media-item')
-      var videoTitle = video.text()
-      var videoId = video.attr('href').split('/')[1]
+    videos.each(() => {
+      const video = $(this).find('J-media-item');
+      const videoTitle = video.text();
+      const videoId = video.attr('href').split('/')[1];
       chapterData.videos.push({
-       title: videoTitle,
-       videoId: videoId
-     })
-    })
-   courseData.push(chapterData)
-
- })
-  return courseData
+        title: videoTitle,
+        videoId
+      });
+    });
+    courseData.push(chapterData);
+  });
+  return courseData;
 }
 
+/**
+ * 打印出课程信息
+ * @param courseData [] 课程数据
+ */
 function printCourseInfo(courseData) {
-  courseData.forEach(function(item) {
-    var chapterTtitle = item.chapterTtitle
-    console.log(chapterTtitle + '\n')
+  courseData.forEach((item) => {
+    // const chapterTtitle = item.chapterTtitle;
+    // console.log(chapterTtitle + '\n');
 
-    item.videos.forEach(function(video) {
-      console.log('【' + video.videoId + '】 :' + video.title + '\n')
-    })
-  })
+    item.videos.forEach((video) => {
+      console.log(`'【'${video.videoId}'】 :${video.title}'\n'`);
+    });
+  });
 }
 
-http.get(url , function(res) {
-  var html = ''
-
-  //ES6箭头函数
-  res.on('data', (data) => html += data)
-  res.on('end' , () => {
-    var courseData = filterChapters(html)
-    printCourseInfo(courseData)
+http.get(url, (res) => {
+  const html = '';
+  // ES6箭头函数
+  res.on('data', data => `${html}${data}`);
+  // 1、以后统一用箭头函数
+  // 2、用 模板字符串 ${html}${data} 来代替 字符串连接，${html}花括号里面放变量，花括号外面放常量
+  res.on('end', () => {
+    // 注：此 HTML 为空，如何构建数据呢？
+    const courseData = filterChapters(html);
+    // console.log('courseData', courseData);
+    printCourseInfo(courseData);
     // console.log(html)
-  })
-  //JS语法
+  });
+  // JS语法
   // res.on('data',function(data) {
   //   html += data
   // })
@@ -69,5 +85,5 @@ http.get(url , function(res) {
   //   console.log(html)
   // })
 }).on('error', () => {
-  console.log('获取课程数据出错！')
-})
+  console.log('获取课程数据出错！');
+});
